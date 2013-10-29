307110f6c8ad5e8ba285d397b24fe729ad01b04a
geoffreykwan <geoffreykwan@gmail.com>
2013-10-29 16:15:32 -0700

    Updated the step type creator to take into consideration changes we have made to step types such as the renderGradingView(), loading jquery dynamically, and saving student work.

--- src/template/TemplateNode.js
+++ src/template/TemplateNode.js
@@ -132,7 +132,7 @@ TemplateNode.prototype.onExit = function() {
  * div id to this function and this function will insert the student data
  * into the div.
  * 
- * @param divId the id of the div we will render the student work into
+ * @param div the div we will render the student work into
  * @param nodeVisit the student work
  * @param childDivIdPrefix (optional) a string that will be prepended to all the 
  * div ids use this to prevent DOM conflicts such as when the show all work div
@@ -145,7 +145,7 @@ TemplateNode.prototype.onExit = function() {
  * look at SensorNode.renderGradingView() as an example of a step that
  * requires additional processing
  */
-TemplateNode.prototype.renderGradingView = function(divId, nodeVisit, childDivIdPrefix, workgroupId) {
+TemplateNode.prototype.renderGradingView = function(displayStudentWorkDiv, nodeVisit, childDivIdPrefix, workgroupId) {
 	/*
 	 * Get the latest student state object for this step
 	 * TODO: rename templateState to reflect your new step type
@@ -169,7 +169,7 @@ TemplateNode.prototype.renderGradingView = function(divId, nodeVisit, childDivId
 	var studentWork = templateState.getStudentWork();
 	
 	//put the student work into the div
-	$('#' + divId).html(studentWork.response);
+	displayStudentWorkDiv.html(studentWork.response);
 };
 
 /**
